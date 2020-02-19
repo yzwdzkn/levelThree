9969a352fda9381ba6cf75ab610779a6ad668a9c
yzwdzkn <1049674046@qq.com>
2020-02-19 16:05:17 +0800

    第一次全部提交

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+    <v-regioncomponet ref='cityAll' v-bind:provinceAll= 'provinceAll' @updateProv='updateProv' @updateCity='updateCity'></v-regioncomponet>
+</div>
+</template>
+<script>
+import Regioncomponet from './components/regioncomponet.vue'
+export default {
+    data () {
+        return{
+            provinceAll:[//省
+                {id:1,name:'浙江省'},
+                {id:2,name:'江苏省'},
+                {id:3,name:'广东省'},
+            ],
+            cityAll:[//市
+                {id:1,name:'杭州市',parentId:1},
+                {id:2,name:'温州市',parentId:1},
+                {id:3,name:'绍兴市',parentId:1},
+                {id:4,name:'苏州市',parentId:2},
+                {id:5,name:'无锡市',parentId:2},
+                {id:6,name:'南京市',parentId:2},
+                {id:7,name:'广州市',parentId:3},
+                {id:8,name:'江门市',parentId:3},
+                {id:9,name:'汕头市',parentId:3},
+            ],
+            areaAll:[//区
+                {id:1,name:'萧山区',parentId:1},
+                {id:2,name:'余杭区',parentId:1},
+                {id:3,name:'龙湾区',parentId:2},
+                {id:4,name:'鹿城区',parentId:2},
+                {id:5,name:'越城区',parentId:3},
+                {id:6,name:'柯桥区',parentId:3},
+                {id:7,name:'吴中区',parentId:4},
+                {id:8,name:'姑苏区',parentId:4},
+                {id:9,name:'惠山区',parentId:5},
+                {id:10,name:'滨湖区',parentId:5},
+                {id:11,name:'玄武区',parentId:6},
+                {id:12,name:'鼓楼区',parentId:6},
+                {id:13,name:'越秀区',parentId:7},
+                {id:14,name:'白云区',parentId:7},
+                {id:15,name:'江海区',parentId:8},
+                {id:16,name:'新会区',parentId:8},
+                {id:17,name:'龙湖区',parentId:9},
+                {id:18,name:'金平区',parentId:9},
+            ],
+            }
+    },
+    components:{
+        'v-regioncomponet':Regioncomponet,
+    },
+    methods:{
+        updateProv(id){
+            console.log('父组件拿到省份id:',id)
+            var city = [];
+                this.cityAll.forEach(function(item,index) {
+                    if(item.parentId==id){
+                        city.push(item)
+                        }
+                });
+                        this.$refs.cityAll.getCity(city)
+            },
+        updateCity(id){
+            console.log('父组件拿到城市id:',id)
+            var area = [];
+                this.areaAll.forEach(function(item,index) {
+                    if(item.parentId==id){
+                        area.push(item)
+                        }
+                });
+                    this.$refs.cityAll.getArea(area)
+            },
+        updateArea(id){
+            console.log('父组件拿到区县id:',id)
+            }
+    }
+    
+}
+</script>
---
+++ src/components/regioncomponet.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <select v-model= "prov" v-on:change="changeProv">
+    <option disabled value="0">请选择省份/地区:</option>
+    <option v-for= 'province in provinceAll' :value= "province.id" >{{province.name}}</option>
+    </select>
+    <select v-model= "city" v-on:change="changeCity">
+    <option disabled value="0">请选择城市</option>
+    <option v-for= "city in cityAll" :value= "city.id">{{city.name}}</option>
+    </select>
+   <select v-model= "area" v-on:change="changeArea">
+    <option disabled value="0">请选择区县</option>
+    <option v-for= "area in areaAll" :value= "area.id">{{area.name}}</option>
+    </select>
+</div>
+
+</template>
+
+<script>
+export default {
+    props:['provinceAll'],
+  data () {
+    return {
+      prov: '',
+      city: '',
+      area: '',
+      cityAll: [],
+      areaAll:[],
+    }
+  },
+  methods:{
+    changeProv(){//改变省会
+      console.log('子组件的id:',this.prov)
+      this.$emit('updateProv',this.prov)//发送请求给父组件的事件 方法名为 'updateProv'  参数为 this.prov
+    },
+    getCity(city){//获取城市
+      console.log('子组件的city:',city)
+      this.cityAll = city;
+    },
+    changeCity(){//改变城市
+      console.log('子组件城市的id：',this.city)
+      this.$emit('updateCity',this.city)//发送请求给父组件的事件 方法名为 'updateCity'  参数为 this.city
+    },
+    getArea(area){//获取区县
+    console.log(area)
+      this.areaAll = area;
+    },
+    changeArea(){//改变区县
+      console.log('子组件区县的id：',this.area)
+      this.$emit('updateArea',this.area)//发送请求给父组件的事件 方法名为 'updateArea'  参数为 this.area
+    }
+  }
+}
+</script>
